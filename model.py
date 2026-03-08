d5ea999186f04ee6d6db95686e5afb0aba364cc5
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2026-03-08 19:14:53 +0900

    Embedding、CNN、BiLSTMクラスをsrc/layers.pyに移動し、モデルクラスを更新

--- model.py
+++ model.py
@@ -1,14 +1,15 @@
 import torch.nn as nn
-from src.embedding import Embedding
-from src.ccn import CNN
+from src.layers import Embedding, CNN, BiLSTM
 
 class Model(nn.Module):
     def __init__(self, vocab_size=1024, embedding_dim=128):
         super().__init__()
         self.embedding = Embedding(vocab_size, embedding_dim)
         self.cnn = CNN(embedding_dim, 100)
+        self.bilstm = BiLSTM(input_size=300, hidden_size=128)
         
     def forward(self, x):
         x = self.embedding(x)
         x = self.cnn(x)
+        x = self.bilstm(x)
         return x
---
+++ src/layers.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+class Embedding(nn.Module):
+    def __init__(self, vocab_size=1024, embedding_dim=128):
+        super(Embedding, self).__init__()
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+    def forward(self, x):
+        return self.embedding(x)
+    
+    
+class CNN(nn.Module):
+    def __init__(self, in_channels, out_channels_per_kernel):
+        super(CNN, self).__init__()
+        self.conv3 = nn.Conv1d(in_channels, out_channels_per_kernel, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv1d(in_channels, out_channels_per_kernel, kernel_size=4, padding=2)
+        self.conv5 = nn.Conv1d(in_channels, out_channels_per_kernel, kernel_size=5, padding=2)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x = x.transpose(1, 2)
+        
+        x3 = self.relu(self.conv3(x)) 
+        x4 = self.relu(self.conv4(x))
+        x5 = self.relu(self.conv5(x))
+        
+        x = torch.cat((x3, x4, x5), dim=1)
+        x = x.transpose(1, 2)
+        return x
+    
+    
+class BiLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        
+    def forward(self, x):
+        output, _ = self.lstm(x)
+        return output
